3ec1e3158bb89be644cdb9ccca50d73b4a3e0092
Halty93 <david.hal@seznam.cz>
2015-09-14 15:28:50 +0200

    Sql: Add primary key drop, unique add, unique drop

---
+++ application/DynamicDB/DynamicDB/Sql/SqlQuery_UniqueDrop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDB.Sql
+{
+    class SqlQuery_UniqueDrop:SqlQuery_withApp
+    {
+        string tableName { get; set; }
+
+        public SqlQuery_UniqueDrop(string applicationName) : base(applicationName)
+        {
+        }
+
+        protected override void BaseExecution(MarshalByRefObject connection)
+        {
+            string parAppName = safeAddParam("applicationName", _applicationName);
+            string parTableName = safeAddParam("tableName", tableName);
+
+            _sqlString = string.Format(
+                "DECLARE @realTableName NVARCHAR(50), @sql NVARCHAR(MAX); exec getRealTableName@{0},@{1}, @realTableName OUTPUT;" +
+                "SET @sql=CONCAT('ALTER TABLE ', @realTableName, 'DROP CONSTRAINT UN_', @realTableName, ';')" +
+                "exec(@sql)",
+                parAppName, parTableName);
+
+            base.BaseExecution(connection);
+        }
+    }
+}
