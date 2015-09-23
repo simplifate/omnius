18d69c609b47b1aa369b2e258a4f7713727a3f64
Dumba <samlachman@centrum.cz>
2015-09-23 11:27:18 +0200

    DB: selectIndex repaired

--- application/DynamicDB/DynamicDB/Sql/SqlQuery_SelectIndexes.cs
+++ application/DynamicDB/DynamicDB/Sql/SqlQuery_SelectIndexes.cs
@@ -9,17 +9,20 @@ namespace DynamicDB.Sql
 {
     public class SqlQuery_SelectIndexes:SqlQuery_withApp
     {
-        protected override void BaseExecution(MarshalByRefObject connection)
+        protected override List<DBItem> BaseExecutionWithRead(MarshalByRefObject connection)
         {
             string parAppName = safeAddParam("applicationName", applicationName);
             string parTableName = safeAddParam("tableName", tableName);
 
             _sqlString = string.Format(
-                "DECLARE @sql VARCHAR(MAX);" +
-                "SET @sql= 'SELECT t.name TableName, i.name IndexName FROM sys.indexes i INNER JOIN sys.tables t ON i.object_id = t.object_id WHERE t.name= @{0} ;'" +
-                "exec (@sql)",parTableName );
-
-            base.BaseExecution(connection);
+                "DECLARE @sql NVARCHAR(MAX), @realTableName NVARCHAR(50);" +
+                "exec getTableRealName @{0}, @{1}, @realTableName output;" +
+                "SET @sql = 'SELECT i.name IndexName FROM sys.indexes i INNER JOIN sys.tables t ON i.object_id = t.object_id WHERE t.name=@realTableName AND i.name IS NOT NULL;';" +
+                "exec sp_executesql @sql, N'@realTableName NVARCHAR(50)', @realTableName;",
+                parAppName, parTableName
+                );
+        
+            return base.BaseExecutionWithRead(connection);
         }
     }
 }
