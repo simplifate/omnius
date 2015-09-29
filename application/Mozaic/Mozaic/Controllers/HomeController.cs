517dd01744815fd69d9426fd404ddbe1f532ab1b
Dumba <samlachman@centrum.cz>
2015-09-29 11:56:49 +0200

    Mozaic: scrap removed

--- application/Mozaic/Mozaic/Controllers/HomeController.cs
+++ application/Mozaic/Mozaic/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mozaic.Models;
 
 namespace Mozaic.Controllers
 {
@@ -10,20 +11,6 @@ namespace Mozaic.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
