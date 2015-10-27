86f064a0c2aa195c73fc5d6c620f6f3c64753bc3
Ondřej Uhlík <zlaty.prciny@seznam.cz>
2015-10-27 14:37:58 +0100

    add file for CheckConfigurationSettings

---
+++ application/FSS.FSPOC.Actions.ReservationSystem/Actions/CheckConfigurationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.FSPOC.Actions.ReservationSystem.Actions
+{
+    class CheckConfigurationSettings
+    {
+    }
+}
