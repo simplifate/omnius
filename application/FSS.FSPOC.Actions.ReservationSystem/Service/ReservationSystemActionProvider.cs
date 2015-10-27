e0d5892bcbfea0e3557799e461f57a9056ce254b
Ondřej Uhlík <zlaty.prciny@seznam.cz>
2015-10-27 13:07:49 +0100

    add file for ReservationSystemActionProvider

---
+++ application/FSS.FSPOC.Actions.ReservationSystem/Service/ReservationSystemActionProvider.cs
@@ -0,0 +1,17 @@
+﻿using FSS.FSPOC.BussinesObjects.Common;
+using FSS.FSPOC.BussinesObjects.Service;
+
+namespace FSS.FSPOC.Actions.ReservationSystem.Service
+{
+    public class ReservationSystemActionProvider :IActionProvider
+    {
+        public int ActionIdFrom => 1;
+
+        public int ActionIdTo => 10;
+
+        public IAction GetAction(int actionId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- application/FSS.FSPOC.BussinesObjects/Service/IActionProvider.cs
+++ application/FSS.FSPOC.BussinesObjects/Service/IActionProvider.cs
@@ -4,8 +4,8 @@ namespace FSS.FSPOC.BussinesObjects.Service
 {
     public interface IActionProvider
     {
-        int ActionIdFrom { get; set; }
-        int ActionIdTo { get; set; }
+        int ActionIdFrom { get;  }
+        int ActionIdTo { get;  }
         IAction GetAction(int actionId);
     }
 }
