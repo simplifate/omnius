0f48027ff75ea237ffca3b105bc2587e4b289d88
Ondřej Uhlík <zlaty.prciny@seznam.cz>
2015-10-27 12:49:42 +0100

    adding method for GetAction

--- application/FSS.FSPOC.BussinesObjects/Service/IActionProvider.cs
+++ application/FSS.FSPOC.BussinesObjects/Service/IActionProvider.cs
@@ -1,8 +1,11 @@
-﻿namespace FSS.FSPOC.BussinesObjects.Service
+﻿using FSS.FSPOC.BussinesObjects.Common;
+
+namespace FSS.FSPOC.BussinesObjects.Service
 {
     public interface IActionProvider
     {
         int ActionIdFrom { get; set; }
         int ActionIdTo { get; set; }
+        IAction GetAction(int actionId);
     }
 }
