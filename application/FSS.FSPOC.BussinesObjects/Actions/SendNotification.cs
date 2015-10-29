91693150693645b7277ba7f92794aaae75bb2355
Ondřej Uhlík <zlaty.prciny@seznam.cz>
2015-10-29 09:34:10 +0100

    Add file for SendNotification

---
+++ application/FSS.FSPOC.BussinesObjects/Actions/SendNotification.cs
@@ -0,0 +1,12 @@
+﻿using FSS.FSPOC.BussinesObjects.Common;
+
+namespace FSS.FSPOC.BussinesObjects.Actions
+{
+    internal class SendNotification : IAction
+    {
+        public ResultAction Run(object paramActin = null)
+        {
+            return new ResultAction();
+        }
+    }
+}
