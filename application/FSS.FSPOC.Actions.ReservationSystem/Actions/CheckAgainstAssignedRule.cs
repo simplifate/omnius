9b836b04405c791b2a8d7778b561a16203463d1b
Ondřej Uhlík <zlaty.prciny@seznam.cz>
2015-10-29 09:28:54 +0100

    Add file for CheckAgainstAssignedRule

---
+++ application/FSS.FSPOC.Actions.ReservationSystem/Actions/CheckAgainstAssignedRule.cs
@@ -0,0 +1,12 @@
+﻿using FSS.FSPOC.BussinesObjects.Common;
+
+namespace FSS.FSPOC.Actions.ReservationSystem.Actions
+{
+    public class CheckAgainstAssignedRule :IAction
+    {
+        public ResultAction Run(object paramActin = null)
+        {
+            return new ResultAction();
+        }
+    }
+}
