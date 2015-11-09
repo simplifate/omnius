c79d812425200fca140af271546fb0ad451aa311
Fabio Melloni <melloni@email.cz>
2015-11-09 19:26:05 +0100

    App manager tile dragging - align to grid, revert when tiles overlap

--- application/FSPOC-WebProject/Scripts/AppManager.js
+++ application/FSPOC-WebProject/Scripts/AppManager.js
@@ -1,5 +1,25 @@
 ﻿$(function () {
     if ($("body.appManagerModule").length) {
-        $(".appPanel").draggable({ snap: true });
+        $(".appPanel").draggable({
+            grid: [120, 120],
+            revert: "invalid",
+            stop: function(){
+                $(this).draggable("option", "revert", "invalid");
+            }
+        });
+        $(".appManagerModule").droppable({
+            tolerance: "fit"
+        });
+        $(".appPanel").droppable({
+            greedy: true,
+            tolerance: "touch",
+            drop: function (event, ui) {
+                ui.draggable.draggable("option", "revert", true);
+            }
+        });
+        $(".appPanel").bind("dragstart", function (event, ui) {
+            ui.originalPosition.top = $(this).position().top;
+            ui.originalPosition.left = $(this).position().left;
+        });
     }
 });
