2439b76a4eaeec9473e56c19e3a10b9006dc04f4
Halty93 <david.hal@seznam.cz>
2015-12-11 16:07:12 +0100

    Persona: application and action Rights

---
+++ application/FSPOC-WebProject/Controllers/Persona/RightsController.cs
@@ -0,0 +1,26 @@
+﻿using FSS.Omnius.Modules.Entitron.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FSPOC_WebProject.Controllers.Persona
+{
+    public class RightsController : Controller
+    {
+        // GET: Rights
+        public ActionResult ActionIndex()
+        {
+            DBEntities e = new DBEntities();
+            return View(e.ActionRights);
+        }
+
+        public ActionResult AppIndex()
+        {
+            DBEntities e = new DBEntities();
+
+            return View(e.ApplicationRights);
+        }
+    }
+}
