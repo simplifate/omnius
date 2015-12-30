fb32792e6b6dd82652a55b58fe0c24dd3cb34f0f
Fabio Melloni <melloni@email.cz>
2015-12-30 12:32:04 +0100

    Hermes - update visual styles

--- application/FSPOC-WebProject/Scripts/Hermes/Init.js
+++ application/FSPOC-WebProject/Scripts/Hermes/Init.js
@@ -3,5 +3,16 @@
         if ($("#smtpMenuArea").length) {
             $("#hermesMenuSMTP").addClass("highlighted");
         }
+        if ($("#templateMenuArea").length) {
+            $("#hermesMenuTemplate").addClass("highlighted");
+        }
+        $("#hermesMenuSMTP").on("click", function () {
+            if (!$("#hermesMenuSMTP").hasClass("highlighted"))
+                window.location.href = "/Hermes/SMTP";
+        });
+        $("#hermesMenuTemplate").on("click", function () {
+            if (!$("#hermesMenuTemplate").hasClass("highlighted"))
+                window.location.href = "/Hermes/Template";
+        });
     }
 });
