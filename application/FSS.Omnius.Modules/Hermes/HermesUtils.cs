57f4657e3c2efe067b08ec498ba3f1816c54411a
Fabio Melloni <melloni@email.cz>
2016-01-15 14:12:45 +0100

    Hermes - add helper function to serialize MailMessage to JSON

---
+++ application/FSS.Omnius.Modules/Hermes/HermesUtils.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Mail;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FSS.Omnius.Modules.Hermes
+{
+    public static class HermesUtils
+    {
+        public static string SerializeMailMessage(MailMessage message, Formatting formatting)
+        {
+            return JsonConvert.SerializeObject(MailMessageToJObject(message), formatting);
+        }
+        public static JObject MailMessageToJObject(MailMessage message)
+        {
+            var result = new JObject();
+            result.Add("From", GetJTokenOrNull(message.From));
+            result.Add("Sender", GetJTokenOrNull(message.Sender));
+            result.Add("ReplyToList", GetJTokenOrNull(message.ReplyToList));
+            result.Add("To", GetJTokenOrNull(message.To));
+            result.Add("Bcc", GetJTokenOrNull(message.Bcc));
+            result.Add("CC", GetJTokenOrNull(message.CC));
+            result.Add("Priority", (int)message.Priority);
+            result.Add("DeliveryNotificationOptions", (int)message.DeliveryNotificationOptions);
+            result.Add("Subject", message.Subject);
+            result.Add("SubjectEncoding", GetJTokenOrNull(message.SubjectEncoding));
+            result.Add("Headers", GetJTokenOrNull(message.Headers));
+            result.Add("HeadersEncoding", GetJTokenOrNull(message.HeadersEncoding));
+            result.Add("Body", message.Body);
+            result.Add("BodyEncoding", GetJTokenOrNull(message.BodyEncoding));
+            result.Add("BodyTransferEncoding", (int)message.BodyTransferEncoding);
+            result.Add("IsBodyHtml", message.IsBodyHtml);
+            return result;
+        }
+        public static JToken GetJTokenOrNull(object input)
+        {
+            if (input != null)
+                return JToken.FromObject(input);
+            else
+                return null;
+        }
+    }
+}
