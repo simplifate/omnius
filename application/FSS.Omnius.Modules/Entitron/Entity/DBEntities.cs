48cb795e9677be05e293369f0eb9851f916a39ee
Dumba <samlachman@centrum.cz>
2016-01-20 10:52:35 +0100

    Persona: Child groups

--- application/FSS.Omnius.Modules/Entitron/Entity/DBEntities.cs
+++ application/FSS.Omnius.Modules/Entitron/Entity/DBEntities.cs
@@ -182,6 +182,11 @@ namespace FSS.Omnius.Modules.Entitron.Entity
                 .WithRequired(e => e.Group)
                 .HasForeignKey(e => e.GroupId);
 
+            modelBuilder.Entity<Group>()
+                .HasMany(e => e.Children)
+                .WithOptional(e => e.Parent)
+                .HasForeignKey(e => e.ParentId);
+
             modelBuilder.Entity<ActionRule>()
                 .HasMany(e => e.ActionRuleRights)
                 .WithRequired(e => e.ActionRule)
--- application/FSS.Omnius.Modules/Entitron/Entity/Persona/Group.cs
+++ application/FSS.Omnius.Modules/Entitron/Entity/Persona/Group.cs
@@ -9,13 +9,24 @@ namespace FSS.Omnius.Modules.Entitron.Entity.Persona
     [Table("Persona_Groups")]
     public partial class Group
     {
+        public Group()
+        {
+            Children = new HashSet<Group>();
+            Users = new HashSet<User>();
+            ActionRuleRights = new HashSet<ActionRuleRight>();
+            ApplicationRights = new HashSet<AppRight>();
+        }
+
         public int Id { get; set; }
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
         [Required]
         public bool IsFromAD { get; set; }
+        public int? ParentId { get; set; }
 
+        public virtual Group Parent { get; set; }
+        public virtual ICollection<Group> Children { get; set; }
         public virtual ICollection<User> Users { get; set; }
         public virtual ICollection<ActionRuleRight> ActionRuleRights { get; set; }
         public virtual ICollection<AppRight> ApplicationRights { get; set; }
