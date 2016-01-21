7445a4e133e6d204499fb803760c68693ac325c2
Dumba <samlachman@centrum.cz>
2016-01-21 13:49:05 +0100

    Log & migration repaired

--- application/FSPOC-WebProject/Global.asax.cs
+++ application/FSPOC-WebProject/Global.asax.cs
@@ -20,6 +20,7 @@ namespace FSPOC_WebProject
             FilterConfig       .RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig        .RegisterRoutes(RouteTable.Routes);
             BundleConfig       .RegisterBundles(BundleTable.Bundles);
+            Logger.Log.Info("Omnius Starts");
         }
     }
 }
