1ad9701c663d0cb866c517628fcc2ee15509d029
Petr Šindelář <petr.sinde@gmail.com>
2016-01-27 15:15:29 +0100

    Creating CssController and UpperPanel

---
+++ application/FSPOC-WebProject/Controllers/Mozaic/CssController.cs
@@ -0,0 +1,23 @@
+﻿using FSS.Omnius.Modules.Entitron.Entity;
+using FSS.Omnius.Modules.Entitron.Entity.Mozaic;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FSPOC_WebProject.Controllers.Mozaic
+{
+    [PersonaAuthorize(Roles = "Admin")]
+    public class CssController : Controller
+    {
+        // GET: Css
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
