79f573ad3de769da8201dcaa5549a0e98f40ab9a
Dumba <samlachman@centrum.cz>
2016-01-27 14:52:04 +0100

    Module Rights

--- application/FSPOC-WebProject/Controllers/PersonaAuthorize.cs
+++ application/FSPOC-WebProject/Controllers/PersonaAuthorize.cs
@@ -9,16 +9,23 @@ namespace System.Web.Mvc
 {
     public class PersonaAuthorizeAttribute : AuthorizeAttribute
     {
+        public string Module { get; set; }
+
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
             CORE core = new CORE();
             User user = filterContext.HttpContext.User.GetLogged(core);
-            if (string.IsNullOrWhiteSpace(Users) && string.IsNullOrWhiteSpace(Roles) && user != null)
+            if (user == null)
+                throw new UnauthorizedAccessException();
+
+            if (!string.IsNullOrWhiteSpace(Module) && !user.canUseModule(Module))
+                throw new UnauthorizedAccessException();
+
+            if (string.IsNullOrWhiteSpace(Users) || Users.Split(' ').Contains(user.username))
                 return;
 
-            if (Users.Split(' ').Contains(user.username))
+            if (string.IsNullOrWhiteSpace(Roles))
                 return;
-
             foreach(string role in Roles.Split(' '))
             {
                 if (user.IsInGroup(role))
