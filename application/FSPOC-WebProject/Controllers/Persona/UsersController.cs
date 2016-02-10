a55182b813c5eb4c57093a54d538782013323077
David Haltuch <david.hal@seznam.cz>
2016-02-10 14:30:29 +0100

    Persona: Users detail added

--- application/FSPOC-WebProject/Controllers/Persona/UsersController.cs
+++ application/FSPOC-WebProject/Controllers/Persona/UsersController.cs
@@ -18,6 +18,12 @@ namespace FSPOC_WebProject.Controllers.Persona
             return View(e.Users);
         }
 
+        public ActionResult Detail(int id)
+        {
+            DBEntities e = new DBEntities();
+            return View(e.Users.SingleOrDefault(x => x.Id == id));
+        }
+
         public ActionResult Create()
         {
 
@@ -27,6 +33,22 @@ namespace FSPOC_WebProject.Controllers.Persona
         public ActionResult Create(User model)
         {
             DBEntities e=new DBEntities();
+
+            //unikátní username, TODO lepší řešení vymyslet přes javascript
+            if (e.Users.Where(x => x.username == model.username).ToList().Count > 0)
+            {
+                TempData["error"] = "Uživatelské jméno již existuje. Změňte prosím své uživatelské jméno.";
+                return RedirectToAction("Create");
+            }
+
+            //user je lokalní, není řešeno přes AD
+            model.isLocalUser = true;
+
+            //datumy se nastavují protože datetime v databázi a v c# mají rozdílné min.hodnoty
+            model.LastLogin = DateTime.Now;
+            model.localExpiresAt = DateTime.Now;
+            model.CurrentLogin = DateTime.Now;
+
             e.Users.Add(model);
             e.SaveChanges();
             return RedirectToAction("Index");
@@ -43,9 +65,10 @@ namespace FSPOC_WebProject.Controllers.Persona
         [HttpPost]
         public ActionResult Edit(User model)
         {
-            DBEntities e=new DBEntities();
+            DBEntities e = new DBEntities();
+
             FSS.Omnius.Modules.Entitron.Entity.Persona.User u = e.Users.SingleOrDefault(x => x.Id == model.Id);
-            e.Users.AddOrUpdate(model,u);
+            e.Users.AddOrUpdate(u, model);
             e.SaveChanges();
 
             return RedirectToAction("Index");
