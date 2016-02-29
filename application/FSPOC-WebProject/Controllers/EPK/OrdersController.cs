c17100fcdfafa7f61ac7060742ac29f2fab31972
Dumba <samlachman@centrum.cz>
2016-02-29 05:34:41 +0100

    EPK - auto-loading

--- application/FSPOC-WebProject/Controllers/EPK/OrdersController.cs
+++ application/FSPOC-WebProject/Controllers/EPK/OrdersController.cs
@@ -51,14 +51,26 @@ namespace FSS.Omnius.Controllers.EPK
         }
         public ActionResult Create()
         {
+            string username = HttpContext.GetLoggedUser().UserName.ToUpper();
             E.Entitron e = HttpContext.GetCORE().Entitron;
             ViewData["dropdownData_dropdown-select26"] = e.GetDynamicTable("Periodicals").Select().ToList().ToDictionary(s => (int)s["id"], s => (string)s["name"]);
             ViewData["dropdownData_dropdown-select27"] = e.GetDynamicTable("Periodical_interval").Select().where(c => c.column("active").Equal(true)).ToList().ToDictionary(s => (int)s["id"], s => (string)s["name"]);
             ViewData["dropdownData_dropdown-select28"] = e.GetDynamicTable("Periodical_forms").Select().where(c => c.column("active").Equal(true)).ToList().ToDictionary(s => (int)s["id"], s => (string)s["name"]);
             ViewData["dropdownData_dropdown-select29"] = e.GetDynamicTable("Periodical_types").Select().where(c => c.column("active").Equal(true)).ToList().ToDictionary(s => (int)s["id"], s => (string)s["name"]);
 
-            E.DBItem user = e.GetDynamicTable("Users").Select().where(c => c.column("ad_id").Equal(HttpContext.GetLoggedUser().UserName)).ToList().FirstOrDefault();
-            ViewData["uic451"] = HttpContext.GetLoggedUser().DisplayName;
+            DBTable users = e.GetDynamicTable("Users");
+            DBItem user = users.Select().where(c => c.column("ad_id").Equal(username)).ToList().FirstOrDefault();
+            DBItem masterUser = user;
+            while (masterUser != null && !string.IsNullOrWhiteSpace(masterUser["h_pernr"].ToString()))
+            {
+                masterUser = users.Select().where(c => c.column("pernr").Equal(masterUser["h_pernr"])).ToList().FirstOrDefault();
+            }
+            DBItem approverUser = users.Select().where(c => c.column("pernr").Equal(user["h_pernr"])).First();
+
+            ViewData["uic451"] = user != null ? $"{user["nachn"]} {user["vorna"]}" : "";
+            ViewData["uic450"] = masterUser != null ? e.GetDynamicTable("Org").Select().where(c => c.column("orgeh").Equal(masterUser["h_orgeh"])).First()["stext"] : "";
+            ViewData["uic460"] = user != null ? user["kostl"] : "";
+            ViewData["uic464"] = approverUser != null ? $"{approverUser["nachn"]} {approverUser["vorna"]}" : "";
 
             ViewData["appIcon"] = "fa-book";
             ViewData["appName"] = "Evidence periodik";
@@ -71,26 +83,34 @@ namespace FSS.Omnius.Controllers.EPK
             if (fc.AllKeys.Contains("uic470"))
             {
                 E.Entitron e = HttpContext.GetCORE().Entitron;
-                E.DBItem user = e.GetDynamicTable("Users").Select().where(c => c.column("ad_id").Equal(HttpContext.GetLoggedUser().UserName)).ToList().FirstOrDefault();
+                DBTable users = e.GetDynamicTable("Users");
+                E.DBItem user = users.Select().where(c => c.column("ad_id").Equal(HttpContext.GetLoggedUser().UserName.ToUpper())).ToList().FirstOrDefault();
                 E.DBItem periodical = e.GetDynamicItem("Periodicals", Convert.ToInt32(fc["uic471"]));
+
+                DBItem masterUser = user;
+                while(masterUser != null && masterUser["h_orgeh"] != null)
+                {
+                    masterUser = users.Select().where(c => c.column("orgeh").Equal(masterUser["h_orgeh"])).First();
+                }
+                DBItem approverUser = users.Select().where(c => c.column("orgeh").Equal(user["h_orgeh"])).First();
+
                 try
                 {
                     E.DBItem item = new E.DBItem();
                     item.createProperty(-1, "id_wf_state", 1);
                     item.createProperty(-3, "id_periodical", fc["uic471"]);
-                    item.createProperty(-4, "purchase_year", DateTime.Now.Year);
+                    item.createProperty(-4, "purchase_year", 2017);
                     item.createProperty(-5, "begin_purchase", fc["uic462"]);
                     item.createProperty(-6, "end_purchase", fc["uic463"]);
                     item.createProperty(-7, "item_count", fc["uic467"]);
-                    item.createProperty(-8, "client_name", user != null ? user["sapid1"] : "");
+                    item.createProperty(-8, "client_name", user != null ? $"{user["nachn"]} {user["vorna"]}" : "");
                     item.createProperty(-9, "client_sapid2", user != null ? user["sapid2"] : "");
-                    item.createProperty(-11, "client_function", "");
-                    item.createProperty(-12, "client_cost_centre", "");
-                    item.createProperty(-13, "client_company_name", "");
-                    item.createProperty(-14, "approver_name", "");
-                    item.createProperty(-15, "approver_sapid2", "");
+                    item.createProperty(-11, "client_function", user != null ? e.GetDynamicTable("Plans").Select().where(c => c.column("orgeh").Equal(user["orgeh"])).First()["stext"] : "");
+                    item.createProperty(-12, "client_cost_centre", user != null ? user["kostl"] : "");
+                    item.createProperty(-13, "client_company_name", masterUser != null ? e.GetDynamicTable("Org").Select().where(c => c.column("orgeh").Equal(masterUser["orgeh"])).First()["stext"] : "");
+                    item.createProperty(-14, "approver_name", approverUser != null ? $"{approverUser["nachn"]} {approverUser["vorna"]}" : "");
+                    item.createProperty(-15, "approver_sapid2", approverUser != null ? approverUser["sapid2"] : "");
                     item.createProperty(-16, "approver_ouid", "");
-                    item.createProperty(-17, "approver_date", DateTime.Now);
                     item.createProperty(-18, "ship_to_address", fc["uic465"]);
                     item.createProperty(-19, "tentatively_net_of_VAT10", Convert.ToInt32(periodical["tentatively_net_of_VAT10"]) * Convert.ToInt32(fc["uic467"]));
                     item.createProperty(-20, "tentatively_net_of_VAT20", Convert.ToInt32(periodical["tentatively_net_of_VAT20"]) * Convert.ToInt32(fc["uic467"]));
