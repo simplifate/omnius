94fb0b09ad7f9c52104555fc54d098470b17cd87
binucz90 <princecz904571@gmail.com>
2016-03-02 16:11:54 +0100

    Export app implemented

--- application/FSPOC-WebProject/Controllers/CORE/ServiceController.cs
+++ application/FSPOC-WebProject/Controllers/CORE/ServiceController.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 using System.IO;
 using System.Collections.Generic;
 using FSS.Omnius.Modules.Entitron.Entity.Master;
+using System.Text;
 
 namespace FSS.Omnius.Controllers.CORE
 {
@@ -14,15 +15,19 @@ namespace FSS.Omnius.Controllers.CORE
     [PersonaAuthorize(Roles = "Admin", Module = "CORE")]
     public class ServiceController : Controller
     {
-  
-        public ActionResult BackupApp(int id)
+
+        public FileStreamResult BackupApp(int id)
         {
             var backupService = new FSS.Omnius.Modules.Entitron.Service.BackupGeneratorService();
+            var context = new DBEntities();
             string jsonText = backupService.ExportApplication(id);
+            var appName = context.Applications.SingleOrDefault(a => a.Id == id).Name;
+            var byteArray = Encoding.UTF8.GetBytes(jsonText);
+            var stream = new MemoryStream(byteArray);
 
-            return Content(jsonText);
+            return File(stream, "text/plain",  appName + ".txt");
+
         }
-
         public ActionResult RecoverApp()
         {
             return View();
