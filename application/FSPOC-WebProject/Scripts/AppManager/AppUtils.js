ec346537e946708863b901fff67a1d8d0f1d6328
Fabio Melloni <melloni@email.cz>
2016-03-04 15:15:36 +0100

    Messages - front end

---
+++ application/FSPOC-WebProject/Scripts/AppManager/AppUtils.js
@@ -0,0 +1,24 @@
+﻿function ShowAppNotification(text, type) {
+    type = type || "info";
+    $("#appNotificationArea .app-alert").remove();
+    switch (type) {
+        case "success":
+            icon = "fa-check";
+            break;
+        case "error":
+            icon = "fa-exclamation-circle";
+            break;
+        case "warning":
+            icon = "fa-exclamation";
+            break;
+        case "info":
+            icon = "fa-info-circle";
+            break;
+    }
+    newNotification = $('<div class="app-alert app-alert-' + type + '"><i class="fa ' + icon + ' alertSymbol"></i>'
+        + text + '<div class="fa fa-times closeAlertIcon"></div></div>');
+    $("#appNotificationArea").append(newNotification);
+    newNotification.find(".closeAlertIcon").on("click", function () {
+        $(this).parents(".app-alert").remove();
+    });
+};
---
+++ application/FSS.Omnius.Modules/Entitron/Entity/Tapestry/TapestryNotification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Entitron.Entity.Tapestry
+{
+    public class TapestryNotification
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+    }
+}
