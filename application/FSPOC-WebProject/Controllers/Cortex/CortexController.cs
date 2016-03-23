95c97e195658da49d435436e488753fcbf60d720
MartinNovak83 <mnvk83@gmail.com>
2016-03-23 11:02:58 +0100

    Cortext - úprava flags a formuláře

--- application/FSPOC-WebProject/Controllers/Cortex/CortexController.cs
+++ application/FSPOC-WebProject/Controllers/Cortex/CortexController.cs
@@ -2,6 +2,7 @@
 using FSS.Omnius.Modules.Entitron.Entity.Cortex;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace FSS.Omnius.Controllers.Cortex
@@ -42,6 +43,62 @@ namespace FSS.Omnius.Controllers.Cortex
             ViewData["DaysInMonthNames"] = daysInMonthNames;
 
             return View("~/Views/Cortex/Form.cshtml");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            DBEntities e = new DBEntities();
+
+            ViewData["ApplicationList"] = e.Applications;
+            ViewData["ScheduleTypeNames"] = scheduleTypeNames;
+            ViewData["DaysNames"] = daysNames;
+            ViewData["MonthsNames"] = monthsNames;
+            ViewData["ModifierNames"] = modifiersNames;
+            ViewData["DaysInMonthNames"] = daysInMonthNames;
+
+            return View("~/Views/Cortex/Form.cshtml", e.Tasks.Single(t => t.Id == id));
+        }
+
+        public ActionResult Save(Task model)
+        {
+            DBEntities e = new DBEntities();
+            if (ModelState.IsValid) {
+                // Záznam již existuje - pouze upravujeme
+                if (!model.Id.Equals(null)) {
+                    Task row = e.Tasks.Single(m => m.Id == model.Id);
+                    row.Active                  = model.Active;
+                    row.AppId                   = model.AppId;
+                    row.Daily_Repeat            = model.Daily_Repeat;
+                    row.Duration                = model.Duration;
+                    row.End_Date                = model.End_Date;
+                    row.End_Time                = model.End_Time;
+                    row.Hourly_Repeat           = model.Hourly_Repeat;
+                    row.Idle_Time               = model.Idle_Time;
+                    row.Minute_Repeat           = model.Minute_Repeat;
+                    row.Monthly_Days            = model.Monthly_Days;
+                    row.Monthly_In_Days         = model.Monthly_In_Days;
+                    row.Monthly_In_Modifiers    = model.Monthly_In_Modifiers;
+                    row.Monthly_Months          = model.Monthly_Months;
+                    row.Name                    = model.Name;
+                    row.Start_Date              = model.Start_Date;
+                    row.Start_Time              = model.Start_Time;
+                    row.Type                    = model.Type;
+                    row.Url                     = model.Url;
+                    row.Weekly_Days             = model.Weekly_Days;
+                    row.Weekly_Repeat           = model.Weekly_Repeat;
+
+                    e.SaveChanges();
+                }
+                else {
+                    e.Tasks.Add(model);
+                    e.SaveChanges();
+                }
+                
+                return RedirectToRoute("Cortex", new { @action = "Index" });
+            }
+            else {
+                return View("~/Views/Cortex/Form.cshtml", model);
+            }
         }
 
         private void MapScheduleTypeNames()
