ecaf82d933e9bcb17eb99ad8283dd27b37ab3076
Fabio Melloni <melloni@email.cz>
2016-03-28 15:19:37 +0200

    Add conversion when saving DateTime

--- application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/CreateDBItemAction.cs
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/CreateDBItemAction.cs
@@ -64,6 +64,8 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
             {
                 if (column.type == "bit")
                     item.createProperty(column.ColumnId, column.Name, vars.ContainsKey($"__Model.{table.tableName}.{column.Name}"));
+                else if (column.type == "datetime")
+                    item.createProperty(column.ColumnId, column.Name, DateTime.Parse((string)vars[$"__Model.{table.tableName}.{column.Name}"]));
                 else if (vars.ContainsKey($"__Model.{table.tableName}.{column.Name}"))
                     item.createProperty(column.ColumnId, column.Name, vars[$"__Model.{table.tableName}.{column.Name}"]);
             }
