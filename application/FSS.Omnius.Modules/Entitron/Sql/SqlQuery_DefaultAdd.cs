99f7f26783e8ee92401dfa84515f6e3203771f9b
Dumba <Dumba@DRIDROOD>
2016-06-02 14:18:36 +0200

    Entitron: Add default value repaired

--- application/FSS.Omnius.Modules/Entitron/Sql/SqlQuery_DefaultAdd.cs
+++ application/FSS.Omnius.Modules/Entitron/Sql/SqlQuery_DefaultAdd.cs
@@ -15,11 +15,11 @@ namespace FSS.Omnius.Modules.Entitron.Sql
         {
             string realTableName = $"Entitron_{application.Name}_{table.tableName}";
             string defaultName = $"DEF_{realTableName}_{column}";
-            string parValName = safeAddParam("value", value);
 
             sqlString =
-                $"ALTER TABLE [{realTableName}] ADD CONSTRAINT [{defaultName}] DEFAULT @{parValName} FOR [{column}];";
-            
+                $"ALTER TABLE [{realTableName}] ADD CONSTRAINT [{defaultName}] DEFAULT '{value}' FOR [{column}];";
+
+
             base.BaseExecution(transaction);
         }
     }
