9866ceaa1ee9ea5f637ac2afa5098bc9d56e5447
Jan Dvorak <honnza@gmail.com>
2016-06-15 13:39:42 +0200

    rename watchtower panels to upper/lower

--- application/FSPOC-WebProject/Scripts/Watchtower/Init.js
+++ application/FSPOC-WebProject/Scripts/Watchtower/Init.js
@@ -6,12 +6,4 @@
     $("#filterSearchMessage").val($("#previousSearchMessage").val());
     $("#filterSearchTimeFrom").val($("#previousSearchTimeFrom").val());
     $("#filterSearchTimeTo").val($("#previousSearchTimeTo").val());
-    $("#showFilterIcon").on("click", function () {
-        $("#filterFormPanelMinimized").hide();
-        $("#filterFormPanel").show();
-    });
-    $("#hideFilterIcon").on("click", function () {
-        $("#filterFormPanel").hide();
-        $("#filterFormPanelMinimized").show();
-    });
 });
