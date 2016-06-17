db6c431b8d6655735eafe78ffe83c3e1069da96b
Fabio Melloni <melloni@email.cz>
2016-06-17 11:44:40 +0200

    CORE Portal - fix partial view

--- application/FSPOC-WebProject/Scripts/Portal/Init.js
+++ application/FSPOC-WebProject/Scripts/Portal/Init.js
@@ -101,21 +101,21 @@ $(function () {
     $("#usersOnlineIndicator").on("click", function () {
         $(".clickableIndicatorRectangle").removeClass("highlighted");
         $("#usersOnlineIndicator").addClass("highlighted");
-        $.get("/Portal/UsersOnline").success(function (result) {
+        $.get("/CORE/Portal/UsersOnline").success(function (result) {
             $("#lowerPanelDynamicContainer").html(result);
         });
     });
     $("#activeProfileIndicator").on("click", function () {
         $(".clickableIndicatorRectangle").removeClass("highlighted");
         $("#activeProfileIndicator").addClass("highlighted");
-        $.get("/Portal/ActiveProfile").success(function (result) {
+        $.get("/CORE/Portal/ActiveProfile").success(function (result) {
             $("#lowerPanelDynamicContainer").html(result);
         });
     });
     $("#activeModulesIndicator").on("click", function () {
         $(".clickableIndicatorRectangle").removeClass("highlighted");
         $("#activeModulesIndicator").addClass("highlighted");
-        $.get("/Portal/ModuleAdmin").success(function (result) {
+        $.get("/CORE/Portal/ModuleAdmin").success(function (result) {
             $("#lowerPanelDynamicContainer").html(result);
             LoadModuleAdminScript();
         });
