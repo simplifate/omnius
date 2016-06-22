cc48c2acb7d59891e53c26bb6b547028e068083a
Dumba <Dumba@DRIDROOD>
2016-06-22 10:16:28 +0200

    generate DB relations

--- application/FSS.Omnius.Modules/Entitron/Sql/SqlQuery_ForeignKeyAdd.cs
+++ application/FSS.Omnius.Modules/Entitron/Sql/SqlQuery_ForeignKeyAdd.cs
@@ -27,23 +27,11 @@ namespace FSS.Omnius.Modules.Entitron.Sql
                 : (onDelete == "null")
                     ? " ON DELETE SET NULL"
                     : (onDelete == "default") ?" ON DELETE SET DEFAULT" : "";
-
-            string parAppName = safeAddParam("applicationName", application.Name);
-            string parTable1Name = safeAddParam("tableName", table.tableName);
-            string parTable2Name = safeAddParam("tableName", table2.tableName);
-            string parForeignKey = safeAddParam("foreignKey", foreignKey);
-            string parPrimaryKey = safeAddParam("primaryKey", primaryKey);
-            string parForeignName = safeAddParam("foreignName", foreignName);
-
-            sqlString = string.Format(
-                "DECLARE @realTable1Name NVARCHAR(50), @realTable2Name NVARCHAR(50), @sql NVARCHAR(MAX);" +
-                "exec getTableRealName @{0}, @{1}, @realTable1Name OUTPUT;" +
-                "exec getTableRealName @{0}, @{2}, @realTable2Name OUTPUT;" +
-                "SET @sql= CONCAT('ALTER TABLE ', @realTable1Name, ' ADD CONSTRAINT FK_', @{3},' FOREIGN KEY (', @{4}, ') REFERENCES ', @realTable2Name, ' (', @{5}, ') " +
-                " {6} {7} ;');" +
-                "exec (@sql);",
-                parAppName, parTable1Name, parTable2Name, parForeignName, parForeignKey, parPrimaryKey, delete, update);
-
+            
+            sqlString =
+                $"ALTER TABLE [Entitron_{application.Name}_{table.tableName}] ADD CONSTRAINT FK_{foreignName} FOREIGN KEY ({foreignKey}) REFERENCES [Entitron_{application.Name}_{table2.tableName}] ({primaryKey}) " +
+                $" {delete} {update} ;";
+            
             base.BaseExecution(transaction);
         }
 
