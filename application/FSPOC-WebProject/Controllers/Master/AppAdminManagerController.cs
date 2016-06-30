b90291f605373de0deef9edb3f04f543723c554c
Jan Dvorak <honnza@gmail.com>
2016-06-30 15:55:58 +0200

    uncomment a flag that was commented for debugging purposes

--- application/FSPOC-WebProject/Controllers/Master/AppAdminManagerController.cs
+++ application/FSPOC-WebProject/Controllers/Master/AppAdminManagerController.cs
@@ -177,7 +177,7 @@ namespace FSS.Omnius.Controllers.Master
                     {
                         var service = new TapestryGeneratorService();
                         blockMapping = service.GenerateTapestry(core, x => Send(x));
-                        //app.TapestryChangedSinceLastBuild = false;
+                        app.TapestryChangedSinceLastBuild = false;
                         Send(Json.Encode(new { id = "tapestry", type = "success", message = "proběhla aktualizace workflow" }));
                     }
                     catch (Exception ex)
