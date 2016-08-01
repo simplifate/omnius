4ff5f6ea684dc69196046ef96f3a4424c054ae99
Skoreto <TomSkorepa@gmail.com>
2016-08-01 15:40:28 +0200

    Remove selection of table while adding new block (Overview)
    The functionality is no more required.

--- application/FSPOC-WebProject/Scripts/Overview/Dialogs.js
+++ application/FSPOC-WebProject/Scripts/Overview/Dialogs.js
@@ -4,7 +4,7 @@
             autoOpen: false,
             resizable: false,
             width: 400,
-            height: 180,
+            height: 140,
             buttons: {
                 "Add": function () {
                     addBlockDialog_SubmitData();
@@ -23,16 +23,10 @@
             },
             open: function () {
                 $(this).find("#block-name").val("");
-                $(this).find("#table-dropdown option").remove();
-                for (i = 1; i < 6; i++) {
-                    fakeTableName = "Table" + i; // TODO: replace with real data - load from a list of FSPOC tables
-                    $(this).find("#table-dropdown").append($('<option value="' + fakeTableName + '">' + fakeTableName + '</option>'));
-                }
             }
         });
         function addBlockDialog_SubmitData() {
             blockName = addBlockDialog.find("#block-name").val();
-            tableName = addBlockDialog.find("#table-dropdown").val();
             newBlock = $('<div class="block"><div class="blockName">' + blockName + '</div><div class="blockInfo"></div></div>');
             $("#overviewPanel .scrollArea").append(newBlock);
             instance.draggable(newBlock, { containment: "parent" });
@@ -53,7 +47,7 @@
             autoOpen: false,
             resizable: false,
             width: 400,
-            height: 180,
+            height: 140,
             buttons: {
                 "Add": function () {
                     addMetablockDialog_SubmitData();
