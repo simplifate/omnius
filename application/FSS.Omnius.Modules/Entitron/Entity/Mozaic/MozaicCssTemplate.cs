14b12de359c32df55b01d6b90345e9059dfb8250
Skoreto <TomSkorepa@gmail.com>
2016-08-15 10:35:08 +0200

    Migration for changing MozaicCssTemplates CSS column to URL

--- application/FSS.Omnius.Modules/Entitron/Entity/Mozaic/MozaicCssTemplate.cs
+++ application/FSS.Omnius.Modules/Entitron/Entity/Mozaic/MozaicCssTemplate.cs
@@ -12,6 +12,6 @@ namespace FSS.Omnius.Modules.Entitron.Entity.Mozaic
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string CSS { get; set; }
+        public string Url { get; set; }
     }
 }
