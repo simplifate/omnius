682297cea70e220cb1eadcf60f3add00295025d6
Michal Šebela <SebelaMichal@hotmail.com>
2016-11-14 08:59:49 +0100

    GetAppUrl action

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/GetAppUrl.cs
@@ -0,0 +1,60 @@
+﻿using FSS.Omnius.Modules.CORE;
+using FSS.Omnius.Modules.Entitron;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
+{
+    [EntitronRepository]
+    public class GetAppUrlAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 1041;
+            }
+        }
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] {  };
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Get App URL";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> invertedVars, Message message)
+        {
+            CORE.CORE core = (CORE.CORE)vars["__CORE__"];
+            string hostname = TapestryUtils.GetServerHostName();
+            string appName = core.Entitron.AppName;
+            outputVars["Result"] = $"{hostname}/{appName}";
+        }
+    }
+}
