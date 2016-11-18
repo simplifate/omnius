9baceae2ffef2ba0d5c693a4754dd897da5b18d5
Michal Šebela <sebelamichal@hotmail.com>
2016-11-18 11:15:15 +0100

    Oprava dotažení jquery.validate do hermes

--- application/FSS.Omnius.Modules/CORE/Services.cs
+++ application/FSS.Omnius.Modules/CORE/Services.cs
@@ -7,10 +7,15 @@ namespace FSS.Omnius.Modules.CORE
     {
         public static string GetFileVersion(string path)
         {
-            string absolutePath = AppDomain.CurrentDomain.BaseDirectory + path;
+            //string absolutePath = AppDomain.CurrentDomain.BaseDirectory + path;
+
+            string p = path.TrimStart('~');
+            p = p.Replace('/', '\\');
+            p = p.TrimStart('\\');
+            string absolutePath = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\') + '\\' + p;
 
             DateTime lastModified = File.GetLastWriteTime(absolutePath);
-            return path + "?version=" + lastModified.Ticks.ToString();
+            return path.TrimStart('~') + "?version=" + lastModified.Ticks.ToString();
         }
     }
 }
