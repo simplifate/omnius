ec07b5eba96e89c8611031dcfd7cb215ec14aba3
Fabio Melloni <melloni@email.cz>
2017-01-17 16:22:36 +0100

    Tapestry - add 2 Persona actions for converting users

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Persona/PersonaIdToRweAction.cs
@@ -0,0 +1,73 @@
+﻿using FSS.Omnius.Modules.CORE;
+using FSS.Omnius.Modules.Entitron.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.other
+{
+    [OtherRepository]
+    class PersonaIdToRweAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 4108;
+            }
+        }
+
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] { "UserId" };
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Persona: UserId to RWE user row";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
+        {
+            CORE.CORE core = (CORE.CORE)vars["__CORE__"];
+            var rweUsersTable = core.Entitron.GetDynamicTable("Users");
+            var context = DBEntities.instance;
+            int userId = (int)vars["UserId"];
+
+            var user = context.Users.Find(userId);
+            var results = rweUsersTable.Select().where(c=> c.column("ad_email").Equal(user.Email)).ToList();
+            if(results.Count > 0)
+            {
+                outputVars["Result"] = results[0];
+            }
+            else
+            {
+                outputVars["Result"] = null;
+            }
+        }
+    }
+}
---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Persona/RweIdToPersonaAction.cs
@@ -0,0 +1,77 @@
+﻿using FSS.Omnius.Modules.CORE;
+using FSS.Omnius.Modules.Entitron;
+using FSS.Omnius.Modules.Entitron.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.other
+{
+    [OtherRepository]
+    class RweIdToPersonaAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 4109;
+            }
+        }
+
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] { "Id", "?Email", "?Pernr", "?SapId", "?SapId2" };
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Persona: RWE property to UserId";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
+        {
+            CORE.CORE core = (CORE.CORE)vars["__CORE__"];
+            var rweUsersTable = core.Entitron.GetDynamicTable("Users");
+            var context = DBEntities.instance;
+            int userId = (int)vars["Id"];
+
+            List<DBItem> results;
+            
+            results = rweUsersTable.Select().where(c => c.column("id").Equal(userId)).ToList();
+            if (results.Count > 0)
+            {
+                string ad_email = (string)results[0]["ad_email"];
+                var userObject = context.Users.SingleOrDefault(u => u.Email == ad_email);
+                outputVars["Result"] = userObject == null ? (int?)null : userObject.Id;
+            }
+            else
+            {
+                outputVars["Result"] = null;
+            }
+        }
+    }
+}
