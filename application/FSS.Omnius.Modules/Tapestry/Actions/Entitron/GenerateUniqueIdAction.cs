15f13c459710035de5f76d3da859f51aa023a596
Fabio Melloni <melloni@email.cz>
2017-01-24 14:43:27 +0100

    New Tapestry action: Generate unique ID

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/GenerateUniqueIdAction.cs
@@ -0,0 +1,65 @@
+﻿using FSS.Omnius.Modules.CORE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.other
+{
+    [OtherRepository]
+    class GenerateUniqueIdAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 1048;
+            }
+        }
+
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] { "TableName", "ColumnName" };
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Generate unique ID";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
+        {
+            CORE.CORE core = (CORE.CORE)vars["__CORE__"];
+            string tableName = (string)vars["TableName"];
+            string columnName = (string)vars["ColumnName"];
+
+            var table = core.Entitron.GetDynamicTable(tableName);
+            var results = table.Select().ToList();
+            int prevoiusId = results.Count > 0 ? results.Select(c => (int)c[columnName]).Max() : 0;
+            outputVars["Result"] = prevoiusId + 1;
+        }
+    }
+}
