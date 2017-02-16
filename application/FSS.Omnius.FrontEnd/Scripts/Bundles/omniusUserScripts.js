526279c417f82dcf7161819a4e3d02dfb0208269
Jakub Senohrábek <admin@kudlaarts.cz>
2017-02-16 16:09:37 +0100

    Removed uncommeted padding that was added to tables everytime and caused overgrowing of panels

--- application/FSS.Omnius.FrontEnd/Scripts/Bundles/omniusUserScripts.js
+++ application/FSS.Omnius.FrontEnd/Scripts/Bundles/omniusUserScripts.js
@@ -875,8 +875,8 @@ $(function () {
             wrapper.css("position", "absolute");
             wrapper.css("left", table.css("left"));
             wrapper.css("top", table.css("top"));
-            wrapper.css("width", tableWidth + 8);
-            table.css("max-width", tableWidth - 8);
+            wrapper.css("width", tableWidth);
+            table.css("max-width", tableWidth);
             table.css("position", "relative");
             table.css("left", "0px");
             table.css("top", "0px");
