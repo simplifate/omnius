9910a24cd32c8ac042dcf3d57f818641f301745e
Fabio Melloni <melloni@email.cz>
2017-02-24 10:44:43 +0100

    Add missing migration files

---
+++ application/FSS.Omnius.Modules/Migrations/201702240918341_Fix IsShared problem.cs
@@ -0,0 +1,22 @@
+namespace FSS.Omnius.Modules.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixIsSharedproblem : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.TapestryDesigner_ResourceItems", "IsShared", c => c.Boolean());
+            AlterColumn("dbo.Tapestry_ResourceMappingPairs", "SourceIsShared", c => c.Boolean());
+            AlterColumn("dbo.Tapestry_ResourceMappingPairs", "TargetIsShared", c => c.Boolean());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Tapestry_ResourceMappingPairs", "TargetIsShared", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.Tapestry_ResourceMappingPairs", "SourceIsShared", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.TapestryDesigner_ResourceItems", "IsShared", c => c.Boolean(nullable: false));
+        }
+    }
+}
