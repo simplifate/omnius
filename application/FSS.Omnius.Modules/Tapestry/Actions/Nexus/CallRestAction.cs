a91bf565da2e2b399eca313409bcd7fc003c8423
Milan Eliáš <milan.elias@futuresolutionsservices.com>
2017-05-29 13:47:18 +0200

    CallRestAction now with exception handling and caching

--- application/FSS.Omnius.Modules/Tapestry/Actions/Nexus/CallRestAction.cs
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Nexus/CallRestAction.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using FSS.Omnius.Modules.CORE;
 using FSS.Omnius.Modules.Tapestry.Actions.Nexus;
 using FSS.Omnius.Modules.Entitron.Entity;
+using FSS.Omnius.Modules.Watchtower;
 using System.Linq;
 using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
 {
-    [NexusRepository]
-    class CallRestAction : Action
+    class CachedResult
     {
+        public DateTime resultTime;
+        public JToken data;
+    }
+
+    [NexusRepository]
+    public class CallRestAction : Action
+    {
+        private static Dictionary<string, CachedResult> cache = new Dictionary<string, CachedResult>();
+
         public override int Id
         {
             get
@@ -27,7 +37,7 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
         {
             get
             {
-                return new string[] { "Endpoint", "Method", "?WSName", "?InputData", "?InputFromJSON" };
+                return new string[] { "Endpoint", "Method", "?WSName", "?InputData", "?InputFromJSON", "?MaxAge" };
             }
         }
 
@@ -45,7 +55,7 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
             {
                 return new string[]
                 {
-                    "Result"
+                    "Result", "Error"
                 };
             }
         }
@@ -60,59 +70,144 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
 
         public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
         {
-            string endpoint = (string)vars["Endpoint"];
-            string method = (string)vars["Method"];
-            string wsName = (string)vars["WSName"];
-            bool inputFromJSON = vars.ContainsKey("WSName") ? (bool)vars["WSName"] : false;
-
-            var context = DBEntities.instance;
-
-            var service = context.WSs.First(c => c.Name == wsName);
-
-            string endpointPath = string.Format("{0}/{1}", service.REST_Base_Url.TrimEnd('/'), endpoint.Trim('/'));
-            var endpointUri = new Uri(endpointPath);
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(endpointUri);
-
-            httpWebRequest.Method = method.ToUpper();
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Accept = "application/json";
-
-            if (!string.IsNullOrEmpty(service.Auth_User))
-                httpWebRequest.Credentials = new NetworkCredential(service.Auth_User, service.Auth_Password);
-
-            if (vars.ContainsKey("InputData"))
+            try
             {
-                string inputJson;
-                if (inputFromJSON)
+                string endpoint = (string)vars["Endpoint"];
+                string method = (string)vars["Method"];
+                string wsName = (string)vars["WSName"];
+                string inputData = vars.ContainsKey("InputData") ? (string)vars["InputData"] : "";
+                int maxAge = vars.ContainsKey("MaxAge") ? (int)vars["MaxAge"] : 0;
+                bool inputFromJSON = vars.ContainsKey("InputFromJSON") ? (bool)vars["InputFromJSON"] : false;
+
+                string keyData = string.Format("{0}_{1}_{2}_{3}_{4}_{5}",
+                            endpoint,
+                            method,
+                            wsName,
+                            inputData,
+                            maxAge.ToString(),
+                            inputFromJSON ? "1" : "0"
+                        );
+                string cacheKey = CalculateMD5(keyData);
+
+                if (maxAge > 0)
                 {
-                    inputJson = (string)vars["InputData"];
+                    if (cache.ContainsKey(cacheKey))
+                    {
+                        CachedResult item = cache[cacheKey];
+                        double diff = (DateTime.UtcNow - item.resultTime).TotalSeconds;
+                        if (diff <= maxAge)
+                        {
+                            outputVars["Result"] = item.data;
+                            return;
+                        }
+                    }
                 }
-                else
+
+                var context = DBEntities.instance;
+                string queryString = "";
+                var service = context.WSs.First(c => c.Name == wsName);
+
+                if (inputData != "" && method.ToUpper() == "GET")
                 {
-                    inputJson = JsonConvert.SerializeObject(vars["InputData"]);
+                    if (inputFromJSON)
+                    {
+                        JToken input = JToken.Parse(inputData);
+                        foreach (JProperty key in input)
+                        {
+                            queryString += key.Name + "=" + (string)key.Value + '&';
+                        }
+                        queryString = queryString.TrimEnd('&');
+                    }
+                    else
+                    {
+                        queryString = inputData;
+                    }
                 }
 
-                byte[] postJsonBytes = Encoding.UTF8.GetBytes(inputJson);
-                httpWebRequest.ContentLength = postJsonBytes.Length;
-                Stream requestStream = httpWebRequest.GetRequestStream();
-                requestStream.Write(postJsonBytes, 0, postJsonBytes.Length);
+                string endpointPath = string.Format("{0}/{1}?{2}", service.REST_Base_Url.TrimEnd('/'), endpoint.Trim('/'), queryString);
+                //var endpointUri = new Uri(endpointPath);
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(endpointPath);
+
+                httpWebRequest.Method = method.ToUpper();
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Accept = "application/json";
+
+                if (!string.IsNullOrEmpty(service.Auth_User))
+                {
+                    httpWebRequest.Credentials = new NetworkCredential(service.Auth_User, service.Auth_Password);
+                }
+
+                if (vars.ContainsKey("InputData") && method.ToUpper() != "GET")
+                {
+                    string inputJson;
+                    if (inputFromJSON)
+                    {
+                        inputJson = (string)vars["InputData"];
+                    }
+                    else
+                    {
+                        inputJson = JsonConvert.SerializeObject(vars["InputData"]);
+                    }
+
+                    byte[] postJsonBytes = Encoding.UTF8.GetBytes(inputJson);
+                    httpWebRequest.ContentLength = postJsonBytes.Length;
+                    Stream requestStream = httpWebRequest.GetRequestStream();
+                    requestStream.Write(postJsonBytes, 0, postJsonBytes.Length);
+                }
+
+                var response = httpWebRequest.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader responseReader = new StreamReader(responseStream);
+                string outputJsonString = responseReader.ReadToEnd();
+
+                if (!string.IsNullOrEmpty(outputJsonString))
+                {
+                    var outputJToken = JToken.Parse(outputJsonString);
+                    outputVars["Result"] = outputJToken;
+                    outputVars["Error"] = false;
+
+                    if (maxAge > 0)
+                    {
+                        if (cache.ContainsKey(cacheKey))
+                        {
+                            cache[cacheKey].resultTime = DateTime.UtcNow;
+                            cache[cacheKey].data = outputJToken;
+                        }
+                        else
+                        {
+                            cache.Add(cacheKey, new CachedResult()
+                            {
+                                resultTime = DateTime.UtcNow,
+                                data = outputJToken
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string errorMsg = e.Message;
+                CORE.CORE core = (CORE.CORE)vars["__CORE__"];
+                WatchtowerLogger.Instance.LogEvent(errorMsg,core.User.Id);
+                outputVars["Result"] = String.Empty;
+                outputVars["Error"] = true;
+            }
+        }
+
+        private string CalculateMD5(string data)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(data);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
             }
 
-            var response = httpWebRequest.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            byte[] buffer = new byte[16 * 1024];
-            MemoryStream ms = new MemoryStream();
-            int read;
-            while ((read = responseStream.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                ms.Write(buffer, 0, read);
-            }
-            string outputJsonString = ms.ToString();
-            if(!string.IsNullOrEmpty(outputJsonString))
-            {
-                var outputJToken = JToken.Parse(outputJsonString);
-                outputVars["Result"] = outputJToken;
-            }
+            return sb.ToString();
         }
     }
 }
