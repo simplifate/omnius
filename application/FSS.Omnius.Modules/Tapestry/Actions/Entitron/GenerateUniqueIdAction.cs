ff2aa17f50893fc7a4d867631dc67494a51f1ea7
mike-elias <milan.elias@futuresolutionservices.com>
2017-06-21 11:54:35 +0200

    Added numerical series option to Generate Unique Id action

--- application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/GenerateUniqueIdAction.cs
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/GenerateUniqueIdAction.cs
@@ -22,7 +22,7 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
         {
             get
             {
-                return new string[] { "TableName", "ColumnName", "?SearchInShared" };
+                return new string[] { "TableName", "ColumnName", "?SearchInShared", "?NumericalSeries" };
             }
         }
 
@@ -55,14 +55,52 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
             CORE.CORE core = (CORE.CORE)vars["__CORE__"];
 
             bool searchInShared = vars.ContainsKey("SearchInShared") ? (bool)vars["SearchInShared"] : false;
-
+            string numSeries = vars.ContainsKey("NumericalSeries") ? (string)vars["NumericalSeries"] : null;
             string tableName = (string)vars["TableName"];
             string columnName = (string)vars["ColumnName"];
 
             var table = core.Entitron.GetDynamicTable(tableName, searchInShared);
             var results = table.Select().ToList();
-            int prevoiusId = results.Count > 0 ? results.Select(c => (int)c[columnName]).Max() : 0;
-            outputVars["Result"] = prevoiusId + 1;
+            var notNullResults = results.Select(c => c[columnName]).Where(c => c != DBNull.Value).Select(c => (int)c).ToList();
+            if (numSeries == null)
+            {
+                int prevoiusId = notNullResults.Count > 0 ? notNullResults.Max() : 0;   
+                outputVars["Result"] = prevoiusId + 1;
+            }
+            else
+            {
+                int previousId;
+                numSeries = numSeries.ToUpper();
+                
+                switch (numSeries)
+                {
+                    case "A": //1000+
+                    case "C":
+                    case "E":
+                    case "P":
+                        previousId = notNullResults.Count > 0 ? notNullResults.Max() : 999;
+                        previousId = previousId < 999 ? 999 : previousId;
+                        break;
+                    case "B": //8000+
+                    case "Z":
+                        previousId = notNullResults.Count > 0 ? notNullResults.Max() : 7999;
+                        previousId = previousId < 7999 ? 7999 : previousId;
+                        break;
+                    case "F": //6000+
+                        previousId = notNullResults.Count > 0 ? notNullResults.Max() : 5999;
+                        previousId = previousId < 5999 ? 5999 : previousId;
+                        break;
+                    case "S": //7000+
+                        previousId = notNullResults.Count > 0 ? notNullResults.Max() : 6999;
+                        previousId = previousId < 6999 ? 6999 : previousId;
+                        break;
+                    default:
+                        previousId = notNullResults.Count > 0 ? notNullResults.Max() : 0;
+                        break;
+                }
+                outputVars["Result"] = previousId + 1;
+
+            }
         }
     }
 }
