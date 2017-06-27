0eb1ef4fb7af3534682404cf919d6b85f6c53637
[Pavel Čihula] <[pavel.cihula@futuresolutionservices.com]>
2017-06-27 11:22:18 +0200

    doporuceni

--- application/FSS.Omnius.FrontEnd/Scripts/Bundles/omniusUserScripts.js
+++ application/FSS.Omnius.FrontEnd/Scripts/Bundles/omniusUserScripts.js
@@ -529,7 +529,6 @@ $(function () {
         });
     $("#uic_zobrazit").on("click", function () {
         $("#uic_panel20").show();
-        $("#uic_panel20").style("top", "auto");
         alert("ok");
     });
 });
