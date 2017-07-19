fb965889edb6188e5b4a404192b01a853e3dd12b
binucz90 <princecz904571@gmail.com>
2017-07-19 14:05:37 +0200

    ConvertJsonStringToJTokenAction

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/ConvertJsonStringToJToken.cs
@@ -0,0 +1,63 @@
+﻿using FSS.Omnius.Modules.CORE;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
+{
+    [EntitronRepository]
+    class ConvertJsonStringToJToken
+    {
+        public class ConvertJsonStringToJTokenAction : Action
+        {
+            public override int Id
+            {
+                get
+                {
+                    return 5020;
+                }
+            }
+
+            public override string[] InputVar
+            {
+                get
+                {
+                    return new string[] { "JsonString" };
+                }
+            }
+
+            public override string Name
+            {
+                get
+                {
+                    return "ConvertJsonStringToJToken";
+                }
+            }
+
+            public override string[] OutputVar
+            {
+                get
+                {
+                    return new string[] { "Result"};
+                }
+            }
+
+            public override int? ReverseActionId
+            {
+                get
+                {
+                    return null;
+                }
+            }
+
+            public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
+            {
+                outputVars["Result"] =  JToken.Parse(vars["JsonString"].ToString());
+
+            }
+        }
+    }
+}
