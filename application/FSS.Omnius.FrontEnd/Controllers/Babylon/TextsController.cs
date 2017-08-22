1e6724f5282edf750345b854a89c0c723cecc380
MartinNovak83 <mnvk83@gmail.com>
2017-08-22 11:42:00 +0200

    * Generátor pot souboru nyní hledá i v ShowMessage akci

--- application/FSS.Omnius.FrontEnd/Controllers/Babylon/TextsController.cs
+++ application/FSS.Omnius.FrontEnd/Controllers/Babylon/TextsController.cs
@@ -7,6 +7,8 @@ using i18n.Domain.Concrete;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using FSS.Omnius.Modules.Entitron.Entity.Master;
+using FSS.Omnius.Modules.Entitron.Entity.Tapestry;
 
 namespace FSS.Omnius.Controllers.Babylon
 {
@@ -26,6 +28,37 @@ namespace FSS.Omnius.Controllers.Babylon
             Dictionary<string, string> data = new Dictionary<string, string>();
             foreach (Page row in db.Pages) {
                 data.Add(row.Id.ToString() + "-" + row.ViewName, row.ViewContent);
+            }
+
+            foreach (Application app in db.Applications) {
+                foreach (TapestryDesignerMetablock mb in db.TapestryDesignerMetablocks.Where(mb => mb.ParentAppId == app.Id)) {
+                    foreach (TapestryDesignerBlock b in mb.Blocks) {
+                        TapestryDesignerBlockCommit lastCommit = b.BlockCommits.OrderByDescending(c => c.Id).FirstOrDefault();
+                        if (lastCommit != null) {
+                            foreach (TapestryDesignerWorkflowRule wf in lastCommit.WorkflowRules) {
+                                List<string> messages = new List<string>();
+                                foreach (TapestryDesignerSwimlane sl in wf.Swimlanes) {
+                                    foreach (TapestryDesignerWorkflowItem item in sl.WorkflowItems) {
+                                        if (item.ActionId == 182) { // Show message action
+                                            string[] vars = item.InputVariables.Split(';');
+                                            foreach(string v in vars) {
+                                                string[] kv = v.Split('=');
+                                                if(kv.Length == 2 && kv[0] == "Message") {
+                                                    if(kv[1].StartsWith("s$")) {
+                                                        messages.Add($"t._(\"{kv[1].Substring(2)}\")");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                if(messages.Count > 0) {
+                                    data.Add($"APP {app.Name} / Blok {b.Name} / Workflow {wf.Name}", string.Join(" ", messages));
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
             var settings = new i18nSettings(new WebConfigSettingService());
