996e5ac64587849f316938d500ad5518ca75ee4c
kamil-bernasek <kamil.bernasek@gmail.com>
2017-08-23 12:58:29 +0200

    ajax homepage JS + 34 36 40 cshtml

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Xmu/ajax.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Xmu/ajax.js
@@ -5,13 +5,13 @@ $(function(){
             var SupportID = $(this).parent().parent().children("td:first-child").html();
             $("#modal_body_ticket").html("");
             $("#modal_name_ticket").html("");
-            $("#preloader").css("display", "block");
+            $("#preloader_ticket").css("display", "block");
             $.ajax({
                 dataType: "html",
                 url: "/Xmu/Ticket?modelId=" + SupportID,
                 success: function (response) {
                     var x = $(response)
-                    $("#preloader").css("display", "none");
+                    $("#preloader_ticket").css("display", "none");
                     $("#modal_body_ticket").html(x.find("#modal_body_ticket").html());
                     $("#modal_name_ticket").html(x.find("#modal_name_ticket").html());
                 }
@@ -21,13 +21,13 @@ $(function(){
             var SupportID = $("#model_id").val();
             $("#modal_body_ticket").html("");
             $("#modal_name_ticket").html("");
-            $("#preloader").css("display", "block");
+            $("#preloader_ticket").css("display", "block");
             $.ajax({
                 dataType: "html",
                 url: "/Xmu/Ticket?modelId=" + SupportID,
                 success: function (response) {
                     var x = $(response)
-                    $("#preloader").css("display", "none");
+                    $("#preloader_ticket").css("display", "none");
                     $("#modal_body_ticket").html(x.find("#modal_body_ticket").html());
                     $("#modal_name_ticket").html(x.find("#modal_name_ticket").html());
                 }
@@ -37,10 +37,39 @@ $(function(){
             $.ajax({
                 type: 'POST',
                 url: '/api/run/Xmu/' + $('#currentBlockName').val() + '/?button=saveSendButton',
-                data: { 'new_comment': $("#new_comment").val(), "TicketID": $("#model_id").val()},
+                data: { 'new_comment': $("#new_comment").val(), "TicketID": $("#model_id").val(), "ticketTag_input": $("#ticketTag_input").val()},
                 success: function (data) {
                     reload();
                 }
             });
         });
+});
+
+
+$(function(){
+  $("#Profiles_table").find(".fa-edit").attr("data-toggle","modal").attr("data-target","#modalProfile");
+  
+  $("#Profiles_table").on("click", ".fa-edit", function () {
+            var UserHash = $(this).parent().parent().children("td:nth-child(3)").html();
+            $("#modal_body_profile").html("");
+            $("#modal_body_cash").html("");
+  			$("#modal_name_profile").html("");
+    		$("#modal_modal_profile").html("");
+            $("#preloader_profile").css("display", "block");
+            $.ajax({
+                type: 'POST',
+                url: "/Xmu/Profile?button=headingHash",
+                data: { 'UserHash': UserHash},
+                success: function (response) {
+                    var x = $(response)
+                    $("#preloader_profile").css("display", "none");
+                    $("#modal_body_profile").html(x.find("#modal_body_profile").html());
+                    $("#modal_modal_profile").html(x.find("#modal_modal_profile").html());
+                    $("#modal_body_cash").html(x.find("#modal_body_cash").html());
+                    $("#modal_name_profile").html(x.find("#modal_name_profile").html());
+                  	var crypto = $("#datatableCrypto");
+                    BootstrapUserInit.DataTable.initTable(crypto);
+                }
+            });
+        }); 
 });
