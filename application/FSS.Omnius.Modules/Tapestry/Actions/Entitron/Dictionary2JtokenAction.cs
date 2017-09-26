4751c39e75d53257f5e09ba4fbf6b4ef49d4c6f6
binucz90 <princecz904571@gmail.com>
2017-09-26 14:34:47 +0200

    dict to jtoken action

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/Dictionary2JtokenAction.cs
@@ -0,0 +1,62 @@
+﻿using FSS.Omnius.Modules.CORE;
+using FSS.Omnius.Modules.Entitron;
+using FSS.Omnius.Modules.Entitron.Entity;
+using FSS.Omnius.Modules.Entitron.Entity.CORE;
+using FSS.Omnius.Modules.Entitron.Sql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
+{
+    [EntitronRepository]
+    public class Dictionary2JtokenAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 10147;
+            }
+        }
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] {"Dictionary"};
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Dictionary To Jtoken";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> invertedVars, Message message)
+        {
+            var InputDictionary = vars["Dictionary"];
+            outputVars["Result"] = JToken.FromObject(InputDictionary);
+        }
+    }
+}
