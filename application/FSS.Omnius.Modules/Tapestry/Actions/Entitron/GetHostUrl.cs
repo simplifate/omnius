c873a1ad9b01185209f98073e45567aa53fed4fc
kamil-bernasek <kamil.bernasek@gmail.com>
2017-10-02 09:53:23 +0200

    new action 1055 getHostUrl

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Entitron/GetHostUrl.cs
@@ -0,0 +1,59 @@
+﻿using FSS.Omnius.Modules.CORE;
+using FSS.Omnius.Modules.Entitron;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.Entitron
+{
+    [EntitronRepository]
+    public class GetHostUrlAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 1055;
+            }
+        }
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] { };
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Get Host URL";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> invertedVars, Message message)
+        {
+            CORE.CORE core = (CORE.CORE)vars["__CORE__"];
+            string hostname = TapestryUtils.GetServerHostName();
+            outputVars["Result"] = $"{hostname}";
+        }
+    }
+}
