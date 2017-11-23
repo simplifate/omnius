86744e6e42e930d490cb93a2171986971f015256
[johnny136] <[john.hammer136@gmail.com]>
2017-11-23 15:28:14 +0100

    pushing JS changes

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/CrossBlockIco.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/CrossBlockIco.js
@@ -1,6 +1,7 @@
 $(function() {
     $("table.klient-table").on("click", "tbody tr", function() {
       	var rowIco = $(this).find("td:nth-child(2)").text();
-      	$('<form class="hiddenForm" method="POST" action="' + window.location.href + '"><input type="hidden" name="Ico" value="' + rowIco + '"/><input type="hidden" name="button" value="klient_table_edit" /></form>').appendTo('body').submit();
+      	alert(rowIco)
+      	$('<form class="hiddenForm" method="POST" action="' + window.location.href + '"><input type="hidden" name="IcoVar" value="' + rowIco + '"/><input type="hidden" name="button" value="klient_table_edit" /></form>').appendTo('body').submit();
     })
 });
