693369f5df204a63dfa6849893fe755e92320f38
kamil-bernasek <kamil.bernasek@gmail.com>
2017-12-01 15:22:47 +0100

    value.js - js for klient edit page. Save all button

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/Value.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/Value.js
@@ -1,3 +1,23 @@
-    if ($("input").each(function (index, element) {
-        $(element).val($(element).attr("value-source"));
-    }));
+$("input").each(function (index, element) {
+    $(element).val($(element).attr("value-source"));
+});
+
+$("#saveAll_button").click(function(){
+  $(".container form").each(function() {
+    if ($(this).parent().is("#profileRow_foreach")) {
+      $.ajax({
+         type: 'POST',
+         url: '/Ifrs9/' + $('#currentBlockName').val() + '?button=saveRowProfile_button',
+         data: $(this).serialize()
+      });
+    }
+    else {
+      $.ajax({
+         type: 'POST',
+         url: '/Ifrs9/' + $('#currentBlockName').val() + '?button=saveRowIbis_button',
+         data: $(this).serialize()
+      });
+    }
+  })
+  window.setTimeout('location.reload()', 1500);
+});
