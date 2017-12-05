8da31a75038f2525ed32f3509b74b60fe34136ad
kamil-bernasek <kamil.bernasek@gmail.com>
2017-12-05 14:54:27 +0100

    js for klient overview

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/CrossBlockIco.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/CrossBlockIco.js
@@ -1,6 +1,6 @@
 $(function() {
-    $("table.klient-table").on("click", "tbody tr", function() {
-      	var rowIco = $(this).find("td:nth-child(2)").text();
-      	$('<form class="hiddenForm" method="POST" action="' + window.location.href + '"><input type="hidden" name="IcoVar" value="' + rowIco + '"/><input type="hidden" name="button" value="klient_table_edit" /></form>').appendTo('body').submit();
+    $("table.klient-table").on("click", "tbody tr i", function() {
+      	var rowIco = $(this).parent().parent().find("td:nth-child(2)").text();
+      	$('<form class="hiddenForm" method="POST" action="' + window.location.href + '"><input type="hidden" name="IcoVar" value="' + rowIco + '"/><input type="hidden" name="button" value="klient_table_modal" /></form>').appendTo('body').submit();
     })
 });
