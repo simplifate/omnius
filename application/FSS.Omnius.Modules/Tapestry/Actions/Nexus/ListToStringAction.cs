9e3cd49e628bb50ed4361054445a13ae9c7b3a2e
kamil-bernasek <kamil.bernasek@gmail.com>
2017-12-15 11:11:43 +0100

    listToString action - this be the new shit

---
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Nexus/ListToStringAction.cs
@@ -0,0 +1,58 @@
+﻿using FSS.Omnius.Modules.CORE;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace FSS.Omnius.Modules.Tapestry.Actions.other
+{
+    [OtherRepository]
+    class ListToStringAction : Action
+    {
+        public override int Id
+        {
+            get
+            {
+                return 3015;
+            }
+        }
+
+        public override string[] InputVar
+        {
+            get
+            {
+                return new string[] { "List", "?Separator" };
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "List to string";
+            }
+        }
+
+        public override string[] OutputVar
+        {
+            get
+            {
+                return new string[] { "Result" };
+            }
+        }
+
+        public override int? ReverseActionId
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
+        {
+            var List = (List<string>)vars["List"];
+            var Separator = vars.ContainsKey("Separator") ? (string)vars["Separator"] : ",";
+            
+            outputVars["Result"] = string.Join(Separator, List.ToArray());
+        }
+    }
+}
