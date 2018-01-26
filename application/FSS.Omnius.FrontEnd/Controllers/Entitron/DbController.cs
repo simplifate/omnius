8209892ed6d81eb37b0e8aa557236400d4cefa91
Dumba <Dumba@DRIDROOD>
2018-01-26 12:35:37 +0100

    Entitron: clear old pool

---
+++ application/FSS.Omnius.FrontEnd/Controllers/Entitron/DbController.cs
@@ -0,0 +1,55 @@
+﻿using E = FSS.Omnius.Modules.Entitron;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FSS.Omnius.Modules.Entitron.Sql;
+using FSS.Omnius.Modules.Entitron;
+
+namespace FSS.Omnius.FrontEnd.Controllers.Entitron
+{
+    public class DbController : Controller
+    {
+        // GET: Db
+        public int ClearOldPool()
+        {
+            try
+            {
+                int count = 0;
+                SqlQuery_Command query = new SqlQuery_Command(E.Entitron.EntitronConnectionString)
+                {
+                    Sql = "DECLARE @v_spid INT " +
+                        "DECLARE c_Users CURSOR " +
+                        "   FAST_FORWARD FOR " +
+                        "   SELECT SPID " +
+                        "   FROM master..sysprocesses (NOLOCK) " +
+                        "   WHERE spid>50  " +
+                        "   AND status='sleeping'  " +
+                        "   AND (program_name = 'EntityFramework' OR program_name = 'Entitron') " +
+                        "   AND DATEDIFF(mi,last_batch,GETDATE())>=60 " +
+                        "   AND spid<>@@spid " +
+
+                        "OPEN c_Users " +
+                        "FETCH NEXT FROM c_Users INTO @v_spid " +
+                        "WHILE (@@FETCH_STATUS=0) " +
+                        "BEGIN " +
+                        "  PRINT 'KILLing '+CONVERT(VARCHAR,@v_spid)+'...' " +
+                        "  EXEC('KILL '+@v_spid) " +
+                        "  FETCH NEXT FROM c_Users INTO @v_spid " +
+                        "END " +
+
+                        "CLOSE c_Users " +
+                        "DEALLOCATE c_Users"
+                };
+                query.Execute(E.Entitron.EntitronConnectionString);
+
+                return count;
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ application/FSS.Omnius.Modules/Entitron/Sql/SqlQuery_Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSS.Omnius.Modules.CORE;
+
+namespace FSS.Omnius.Modules.Entitron.Sql
+{
+    public class SqlQuery_Command : SqlQuery_withoutApp
+    {
+        public string Sql { get; set; }
+
+        public SqlQuery_Command(string connection) : base(connection)
+        {
+        }
+
+        protected override void BaseExecution(MarshalByRefObject connection)
+        {
+            sqlString = Sql;
+
+            base.BaseExecution(connection);
+        }
+        protected override ListJson<DBItem> BaseExecutionWithRead(MarshalByRefObject connection)
+        {
+            sqlString = Sql;
+
+            return base.BaseExecutionWithRead(connection);
+        }
+    }
+}
