729d517650a1e192b58fe82c1a3827683cb164f7
binucz90 <princecz904571@gmail.com>
2018-01-31 10:22:10 +0100

    marketdeph action update currencypair

--- application/FSS.Omnius.Modules/Tapestry/Actions/Apps/XMU/MarketDeph Action.cs
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Apps/XMU/MarketDeph Action.cs
@@ -27,7 +27,7 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
         {
             get
             {
-                return new string[] {"IpAddress","Port", "Market"};
+                return new string[] {"IpAddress","Port","CurencyPair"};
             }
         }
 
@@ -58,7 +58,8 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
         public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
         {
             CORE.CORE core = (CORE.CORE)vars["__CORE__"];
-            string initJson = $"{{\"jsonrpc\": \"2.0\", \"method\": \"init\", \"params\": {{\"market\" : \"btcusd\"}}, \"id\": {requestId++}}}";
+            string pair = vars["CurencyPair"].ToString();
+            string initJson = $"{{\"jsonrpc\": \"2.0\", \"method\": \"init\", \"params\": {{\"market\" : \"{pair}\"}}, \"id\": {requestId++}}}";
             string inputJson =
                     $"{{\"jsonrpc\": \"2.0\", \"method\": \"Orderbook.get\", \"params\": [], \"id\": {requestId++ + 1}}}";
             string ipAddress = vars["IpAddress"].ToString();
