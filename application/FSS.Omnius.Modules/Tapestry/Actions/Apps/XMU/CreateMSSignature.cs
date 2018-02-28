eebabda4f270a5e44a6403f7c3e4c0a0041c1c0c
Milan Eliáš <milan.elias@futuresolutionservices.com>
2018-02-28 14:52:10 +0100

    MS signature upgrade - keys loaded from web config + 2 possible controller types

--- application/FSS.Omnius.Modules/Tapestry/Actions/Apps/XMU/CreateMSSignature.cs
+++ application/FSS.Omnius.Modules/Tapestry/Actions/Apps/XMU/CreateMSSignature.cs
@@ -7,6 +7,7 @@ using Org.BouncyCastle.Utilities.IO.Pem;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Utilities.Encoders;
+using System.Configuration;
 
 namespace FSS.Omnius.Modules.Tapestry.Actions.other
 {
@@ -15,31 +16,33 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
     {
         public override int Id => 1985;
 
-        public override string[] InputVar => new string[] { "s$Data" };
+        public override string[] InputVar => new string[] { "s$Data","?s$Controller[][config|money]" };
 
         public override string Name => "Create MoneyServer signature";
 
         public override string[] OutputVar => new string[] { "Result" };
 
         public override int? ReverseActionId => null;
+        
 
-        private const string XMUPrivateKey = @"-----BEGIN PRIVATE KEY-----
-MIHuAgEAMBAGByqGSM49AgEGBSuBBAAjBIHWMIHTAgEBBEIAxqQzDeo8g4qn07/l
-SHBpaRCalQ2x/I9QhE4poHK5pTG5XZLQjmrD1qvJeJzd1SOq/PwmcTuvvhubMMmP
-f1P09wShgYkDgYYABABGiLsZe68bflN/obCraQvIB53O5WyDt5z8Ji6QoRbB0UOg
-cqGWGqL8Tg2JU8rums+wO3hdBmn9t7nqr8telsQUHwH6DLeXvel7PATynY3Kj3Og
-sE0FP1G8FoMc/5V0lRLGpwyUfH8rT9H/VYC4l2gM58TCmEZ4mu1ygy7mg7vhDZ4d
-2A==
------END PRIVATE KEY-----";
 
         public override void InnerRun(Dictionary<string, object> vars, Dictionary<string, object> outputVars, Dictionary<string, object> InvertedInputVars, Message message)
         {
+            string controller = vars.ContainsKey("Controller") ? ((string)vars["Controller"]).ToLower() : "config";
+            string privateKey = controller == "config" ? ConfigurationManager.AppSettings["MS_configController"].ToString() : ConfigurationManager.AppSettings["MS_moneyController"].ToString();
+            if (controller == "config")
+                privateKey = ConfigurationManager.AppSettings["MS_configController"].ToString();
+            else if (controller == "money")
+                privateKey = ConfigurationManager.AppSettings["MS_moneyController"].ToString();
+            else
+                throw new InvalidParameterException($"CreateMSSignatureAction: Invalid controller type {controller}");
+
             string timestamp = GetCurrentTimestamp();
             string body = vars.ContainsKey("Data") ? (string)vars["Data"] : "";
 
             byte[] data = Encoding.UTF8.GetBytes(string.Format("{0}@{1}", body, timestamp));
 
-            StringReader reader = new StringReader(XMUPrivateKey);
+            StringReader reader = new StringReader(privateKey);
             PemObject pem = (new PemReader(reader)).ReadPemObject();
             AsymmetricKeyParameter key = PrivateKeyFactory.CreateKey(pem.Content);
 
