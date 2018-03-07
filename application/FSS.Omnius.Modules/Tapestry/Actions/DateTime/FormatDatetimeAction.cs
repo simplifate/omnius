86d67b4de66b89db6ce531f6b33027d5968f85f4
binucz90 <princecz904571@gmail.com>
2018-03-07 10:53:33 +0100

    format datetime update

--- application/FSS.Omnius.Modules/Tapestry/Actions/DateTime/FormatDatetimeAction.cs
+++ application/FSS.Omnius.Modules/Tapestry/Actions/DateTime/FormatDatetimeAction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using FSS.Omnius.Modules.CORE;
+using System.Globalization;
 
 namespace FSS.Omnius.Modules.Tapestry.Actions.other
 {
@@ -13,7 +14,7 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
 
         public override string Name => "Format DateTime";
 
-        public override string[] OutputVar => new string[] { "Result" };
+        public override string[] OutputVar => new string[] { "Result","Error"};
 
         public override int? ReverseActionId => null;
 
@@ -21,8 +22,20 @@ namespace FSS.Omnius.Modules.Tapestry.Actions.other
         {
             DateTime dateTime = (DateTime)vars["Input"];
             string format = vars.ContainsKey("Format") ? (string)vars["Format"] : "o";
+            CultureInfo provider = CultureInfo.InvariantCulture;
 
-            outputVars["Result"] = dateTime.ToLocalTime().ToString(format);
+            try
+            {
+                outputVars["Result"] = DateTime.ParseExact(vars["Input"].ToString(), format, provider).ToString();
+                outputVars["Error"] = false;
+            }
+            catch (FormatException)
+            {
+                outputVars["Result"] = null;
+                outputVars["Error"] = true;
+            }
+
+
         }
     }
 }
