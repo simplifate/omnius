4767d5911a697386b30d49180f0e58d701127334
binucz90 <princecz904571@gmail.com>
2018-03-20 12:37:56 +0100

    error message

--- application/FSS.Omnius.FrontEnd/Global.asax.cs
+++ application/FSS.Omnius.FrontEnd/Global.asax.cs
@@ -49,7 +49,7 @@ namespace FSS.Omnius.FrontEnd
                 foreach (Exception error in Context.AllErrors)
                 {
                     Omnius.Modules.Watchtower.OmniusException.Log(
-                        $"Global: {Request?.HttpMethod} {Request?.Url.AbsoluteUri}",
+                        error.Message,
                         Omnius.Modules.Watchtower.OmniusLogSource.CORE,
                         innerException: error);
                 }
