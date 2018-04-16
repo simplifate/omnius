a762feed1586d4abd28be47e519d114ccaf5b429
[Pavel Čihula] <[pavel.cihula@futuresolutionservices.com]>
2018-04-16 08:37:21 +0200

    vozovpark js

---
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/VozovyPark/selectCategory.js
@@ -0,0 +1,38 @@
+$(function () {
+    $("#Profiles_table").find(".fa-edit").attr("data-toggle", "modal").attr("data-target", "#modalProfile");
+
+    $("#Profiles_table").on("click", ".fa-edit", function () {
+        var userId = $(this).parent().parent().children("td:nth-child(2)").text();
+        $("#modal_body_profile").html("");
+        $("#modal_name_profile").html("");
+        $("#preloader_profile").css("display", "block");
+        $.ajax({
+            type: 'GET',
+            url: "/Vozovypark/VozovyParkNew?modelId=" + userId,
+            success: function (response) {
+                var x = $(response)
+                $("#preloader_profile").css("display", "none");
+                $("#modal_body_profile").html(x.find("#modal_body_profile").html());
+                $("#modal_name_profile").html(x.find("#modal_name_profile").html());
+            }
+        });
+    });
+});
+$(function () {
+    var reload = function () {
+        var userId = $("#hiddenUserId").val();
+        $("#modal_body_profile").html("");
+        $("#modal_name_profile").html("");
+        $("#preloader_profile").css("display", "block");
+        $.ajax({
+            dataType: "html",
+            url: "/Vozovypark/VozovyParkNew?modelId=" + userId,
+            success: function (response) {
+                var x = $(response)
+                $("#preloader_profile").css("display", "none");
+                $("#modal_body_profile").html(x.find("#modal_body_profile").html());
+                $("#modal_name_profile").html(x.find("#modal_name_profile").html());
+            }
+        });
+    };
+});
