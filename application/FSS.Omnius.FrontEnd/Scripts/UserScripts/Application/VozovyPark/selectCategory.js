3fd63cd70bb6ea5a21b1a08095b25f972e01c9a4
martin <martin.novak@futuresolutionservices.com>
2018-04-19 10:56:55 +0200

    * vozový park ajax

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/VozovyPark/selectCategory.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/VozovyPark/selectCategory.js
@@ -1,5 +1,45 @@
-$(document).ready(function(){
-    $("kategorie").change(function(){
-		alert("Vybral jsi zemědělce")
+var CarCategorySelect = {
+  
+  init: function() {
+    $(document).on('change', '#kategorie', $.proxy(this._onCategoryChange, this));
+  },
+  
+  
+  setData: function(data)
+  {
+    console.log(data);
+  },
+  
+  loadData: function(selectedCategory, token)
+  {
+    $.ajax({
+      url: '/api/run/VozovyPark/Prehled?button=Cars',
+      type: 'POST',
+      data: {
+        '__RequestVerificationToken': token,
+        'kategorie': selectedCategory,
+        'button': 'Cars'
+      },
+      success: $.proxy(this.setData, this)
     });
-});
+  },
+  
+  getAntiForgeryToken: function(selectedCategory)
+  {
+    var self = this;
+    $.ajax({
+      url: '/Persona/Account/GetAntiForgeryToken',
+      type: 'GET',
+      success: function(token) {
+        self.loadData(selectedCategory, token);
+      }
+    });
+  },
+  
+  _onCategoryChange: function(event)
+  {
+    this.getAntiForgeryToken(event.target.value);
+  }
+};
+
+$($.proxy(CarCategorySelect.init, CarCategorySelect));
