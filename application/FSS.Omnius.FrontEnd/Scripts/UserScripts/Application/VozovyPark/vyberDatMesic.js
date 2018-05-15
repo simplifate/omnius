049526dea04c2ba8eef33f8644def28172eff26b
[Pavel Čihula] <[pavel.cihula@futuresolutionservices.com]>
2018-05-15 09:46:57 +0200

    datamesicJSaCSHTML

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/VozovyPark/vyberDatMesic.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/VozovyPark/vyberDatMesic.js
@@ -1,14 +1,14 @@
 $(function() {
       $(".notesu").each(function() {
-        var modelId = $(this).attr('month');
+        var modelId = $(this).attr('id');
         var item = $(this);
         $.ajax({
           type: 'GET',
           url: '/VozovyPark/DataDate?modelId=' + modelId,
           data: {},
-          success: function (data) {
+          success: function (dataq) {
             var x = $(data)
-            $(item).html(x.find("#accordion-2").html());
+            $(item).html(x.find("#accordion-3").html());
       }
     });
       });
