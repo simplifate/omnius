9c309fdc2df69ee40212f279728d0411b5cc4ab1
[Pavel Čihula] <[pavel.cihula@futuresolutionservices.com]>
2018-05-29 14:52:44 +0200

    jsProfileusers

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Xmu/funkce.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Xmu/funkce.js
@@ -1,4 +1,5 @@
 $(function(){
+  alert("bagr");
   $("#Profiles_table").find(".fa-edit").attr("data-toggle","modal").attr("data-target","#modalProfile");
   
   $("#Profiles_table").on("click", ".fa-edit", function () {
@@ -53,31 +54,6 @@ $(function () {
     }, 1000);
 });
 
-/*
-var transactionColors = function () {
-    setTimeout(function () {
-        $("#Transactions_table tbody tr").each(function () {
-            var flag = $(this).children("td:nth-child(3)").text();
-            if (flag == "True") {
-                $(this).css("background-color", "#ffdede")
-            }
-        })
-    }, 500);
-}
-
-$(function () {
-    $("tfoot input").on("blur", function () { transactionColors() });
-});
-
-$(function () {
-    $("#Transactions_table").parent().on("click", ".paginate_button", function () { transactionColors() });
-    $("#Transactions_table").parent().on("click", ".sorting", function () { transactionColors() });
-    $("#Transactions_table").parent().on("click", ".sorting_asc", function () { transactionColors() });
-    $("#Transactions_table").parent().on("click", ".sorting_desc", function () { transactionColors() });
-    $("#Transactions_table").parent().keyup("tfoot input", function () { transactionColors() });
-});
-*/
-
 $(function(){
   $("#ExchangeVOther_table").find(".fa-sign-in").attr("data-toggle","modal").attr("data-target","#modalPair");
   
@@ -105,46 +81,6 @@ $(function(){
         });
   });
 });
-$(function(){
-    var reloadImage = function () {
-            var UserHash = $("#headingHash").html();
-            $("#modal_body_profile").html("");
-            $("#modal_body_cash").html("");
-  			$("#modal_name_profile").html("");
-    		$("#modal_modal_profile").html("");
-            $("#preloader_profile").css("display", "block");
-
-            $.ajax({
-                url: '/Persona/Account/GetAntiForgeryToken',
-                type: 'GET',
-                success: function (token) {
-                    $.ajax({
-                        type: 'POST',
-                        url: "/Xmu/Profile?button=headingHash",
-                        data: { 'UserHash': UserHash, '__RequestVerificationToken': token },
-                        success: function (response) {
-                            var x = $(response)
-                            $("#preloader_profile").css("display", "none");
-                            $("#modal_body_profile").html(x.find("#modal_body_profile").html());
-                            $("#modal_modal_profile").html(x.find("#modal_modal_profile").html());
-                            $("#modal_body_cash").html(x.find("#modal_body_cash").html());
-                            $("#modal_name_profile").html(x.find("#modal_name_profile").html());
-                          	$("#inputDate").datetimepicker({
-                                datepicker: true,
-                                timepicker: false,
-                                format: "d.m.Y"
-                            }).off('mousewheel');
-                            $("#modalProfile #Support_request_table").find(".fa-edit").attr("data-toggle", "modal").attr("data-target", "#modalTicket");
-                            var tables = $("#actualTransactions table");
-                            tables.each(function () {
-                                var table = $(this);
-                                BootstrapUserInit.DataTable.initTable(table);
-                            });
-                        }
-                    });
-                }
-            });
-        }
 
     $(document).on("click", "#deleteImage", function (e) {
       	var bui = BootstrapUserInit;
