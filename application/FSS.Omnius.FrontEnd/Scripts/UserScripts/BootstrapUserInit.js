85f4b02c4bc020b2e99c50d569e0e46fc4516673
binucz90 <princecz904571@gmail.com>
2018-07-27 11:05:27 +0200

    datatable page length 20 default

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/BootstrapUserInit.js
+++ application/FSS.Omnius.FrontEnd/Scripts/UserScripts/BootstrapUserInit.js
@@ -111,7 +111,7 @@
                     } : false,
                     //order: [[ 1, 'asc' ]],
                     paging: table.data('dtpaging') == '1',
-                    pageLength: 50,
+                    pageLength: 20,
                     lengthMenu: [[10, 20, 50, 100, 200, 500, 1000, -1], [10, 20, 50, 100, 200, 500, 1000, 'Vše']],
                     info: table.data('dtinfo') == '1',
                     filter: table.data('dtfilter') == '1' || table.data('dtcolumnfilter') == '1',
