398c7897bc6829506a58c158a8ecf4cff6756135
Samuel Lachman <samlachman@centrum.cz>
2018-08-08 14:09:58 +0200

    Seed: basics

--- application/FSS.Omnius.Modules/Migrations/Configuration.cs
+++ application/FSS.Omnius.Modules/Migrations/Configuration.cs
@@ -14,33 +14,35 @@ namespace FSS.Omnius.Modules.Migrations
 
         protected override void Seed(Entitron.Entity.DBEntities context)
         {
-            //  This method will be called after migrating to the latest version.
-            //if (!context.Modules.Any())
-            //    context.Modules.AddOrUpdate(
-            //        new Entitron.Entity.CORE.Module { Name = "CORE", Description = "Central Omnius Rellay Engine", IsEnabled = true },
-            //        new Entitron.Entity.CORE.Module { Name = "Entitron", Description = "Manage Database", IsEnabled = true },
-            //        new Entitron.Entity.CORE.Module { Name = "Mozaic", Description = "Rendering Html", IsEnabled = true },
-            //        new Entitron.Entity.CORE.Module { Name = "Tapestry", Description = "WorkFlow", IsEnabled = true },
-            //        new Entitron.Entity.CORE.Module { Name = "Persona", Description = "Manage Rights, Users, Group", IsEnabled = true }
-            //    );
-
+            // Enum: Workflow types
             if (!context.WorkFlowTypes.Any())
                 context.WorkFlowTypes.AddOrUpdate(
                     new Entitron.Entity.Tapestry.WorkFlowType { Name = "Init" },
                     new Entitron.Entity.Tapestry.WorkFlowType { Name = "Partial" },
                     new Entitron.Entity.Tapestry.WorkFlowType { Name = "Preview" }
                 );
-
+            // Enum: Actor types
             if (!context.Actors.Any())
                 context.Actors.AddOrUpdate(
                     new Entitron.Entity.Tapestry.Actor { Name = "Manual" },
                     new Entitron.Entity.Tapestry.Actor { Name = "Time" },
                     new Entitron.Entity.Tapestry.Actor { Name = "Auto" }
                 );
-            
+
+            // system default application
+            var systemApp = context.Applications.SingleOrDefault(a => a.IsSystem) ?? new Entitron.Entity.Master.Application { Name = "System", DisplayName = "System", IsSystem = true, IsPublished = false, IsEnabled = true, TitleFontSize = 12, Color = 1, TileHeight = 5, TileWidth = 5 };
             if (!context.Applications.Any())
-                context.Applications.AddOrUpdate(
-                    new Entitron.Entity.Master.Application { Name = "System", DisplayName = "System", IsSystem = true, IsPublished = false, IsEnabled = true, TitleFontSize = 12, Color = 1, TileHeight = 5, TileWidth = 5 }
+                context.Applications.AddOrUpdate(systemApp);
+            // system default AD group
+            if (!context.ADgroups.Any())
+                context.ADgroups.AddOrUpdate(
+                    new Entitron.Entity.Persona.ADgroup { Name = "System", Application = systemApp, isAdmin = true }
+                );
+
+            // empty shared dbscheme
+            if (!context.DBSchemeCommits.Any())
+                context.DBSchemeCommits.AddOrUpdate(
+                    new Entitron.Entity.Entitron.DbSchemeCommit { Application = systemApp, CommitMessage = "Empty", IsComplete = true, Timestamp = DateTime.UtcNow }
                 );
         }
     }
