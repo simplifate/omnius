e485c0f76ac9836d2c81e0deca093b3fda2596f6
Samuel Lachman <samlachman@centrum.cz>
2018-08-08 14:49:20 +0200

    Seed: admin

--- application/FSS.Omnius.Modules/Migrations/Configuration.cs
+++ application/FSS.Omnius.Modules/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 namespace FSS.Omnius.Modules.Migrations
 {
+    using FSS.Omnius.Modules.Entitron.Entity.Persona;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -34,16 +36,28 @@ namespace FSS.Omnius.Modules.Migrations
             if (!context.Applications.Any())
                 context.Applications.AddOrUpdate(systemApp);
             // system default AD group
+            var systemAD = context.ADgroups.SingleOrDefault(ad => ad.Name == "System") ?? new ADgroup { Name = "System", Application = systemApp, isAdmin = true };
             if (!context.ADgroups.Any())
-                context.ADgroups.AddOrUpdate(
-                    new Entitron.Entity.Persona.ADgroup { Name = "System", Application = systemApp, isAdmin = true }
-                );
+                context.ADgroups.AddOrUpdate(systemAD);
 
             // empty shared dbscheme
             if (!context.DBSchemeCommits.Any())
                 context.DBSchemeCommits.AddOrUpdate(
                     new Entitron.Entity.Entitron.DbSchemeCommit { Application = systemApp, CommitMessage = "Empty", IsComplete = true, Timestamp = DateTime.UtcNow }
                 );
+
+            // admin account
+            if (!context.Users.Any())
+            {
+                var store = new UserStore<User, Iden_Role, int, UserLogin, Iden_User_Role, UserClaim>(context);
+                var manager = new UserManager<User, int>(store);
+                var user = new User { UserName = "admin", DisplayName = "Admin", Email = "admin@example.com", isLocalUser = true, localExpiresAt = DateTime.UtcNow, LastLogin = DateTime.UtcNow, LastLogout = DateTime.UtcNow, CurrentLogin = DateTime.UtcNow };
+
+                manager.Create(user, "pass132");
+
+                user.ModuleAccessPermission = new ModuleAccessPermission { Athena = true, Babylon = true, Core = true, Cortex = true, Entitron = true, Hermes = true, Master = true, Mozaic = true, Nexus = true, Persona = true, Sentry = true, Tapestry = true, Watchtower = true, User = user };
+                user.ADgroup_Users.Add(new ADgroup_User { ADgroup = systemAD, User = user });
+            }
         }
     }
 }
