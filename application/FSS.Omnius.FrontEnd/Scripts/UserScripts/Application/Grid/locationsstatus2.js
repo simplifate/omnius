bc0f06d3f82752d4d51213072a72cf82cb3c3ad1
Samuel Lachman <samlachman@centrum.cz>
2018-09-20 14:56:09 +0200

    remove apps

--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Grid/locationsstatus2.js
+++
@@ -1,6 +0,0 @@
-       $(function() { $("#LocationsDropdown").on('change', function () {
-         	$("#chooselocation").click();
-            var val = this.val;
-         	var form = $('<form class="hiddenForm" method="POST" action="' + window.location.href + '"><input type="hidden" name="modelId" value="this.value" /><input type="hidden" name="button" value="#chooselocation" /></form>');
-        });
-                    });
--- application/FSS.Omnius.FrontEnd/Scripts/UserScripts/Application/Ifrs9/CrossBlockIco.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $("table.klient-table").on("click", "tbody tr i", function() {
-      	var rowIco = $(this).parent().parent().find("td:nth-child(2)").text();
-      	$('<form class="hiddenForm" method="POST" action="' + window.location.href + '"><input type="hidden" name="IcoVar" value="' + rowIco + '"/><input type="hidden" name="button" value="klient_table_modal" /></form>').appendTo('body').submit();
-    })
-});
